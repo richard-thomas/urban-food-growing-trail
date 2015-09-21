a7482395a99484c1a3d62d87cb1b6eaa2d3bde38
Audium <Audium@RichThomas-D630.mshome.net>
2015-09-21 17:09:47 +0100

    Switch to separate info file + auto-generated buttons

---
+++ public_html/trail_geojson.js
@@ -0,0 +1,223 @@
+trailGeoJson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.5805962,
+                    51.4493936,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "tm-plat3",
+                "styleUrl": "#icon-503-DB4436-nodesc",
+                "styleHash": "-3b37c65d"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.5825918,
+                    51.4490994,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "tm-outdoor-beds",
+                "styleUrl": "#icon-503-DB4436-nodesc",
+                "styleHash": "-3b37c65d"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.5862074,
+                    51.449514,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "temple-st-100",
+                "styleUrl": "#icon-503-DB4436-nodesc",
+                "styleHash": "-3b37c65d"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.5884323,
+                    51.449061,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "chatterton-birthplace",
+                "styleUrl": "#icon-503-DB4436-nodesc",
+                "styleHash": "-3b37c65d"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.5899263,
+                    51.4486598,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "mary-redcliffe",
+                "styleUrl": "#icon-503-DB4436-nodesc",
+                "styleHash": "-3b37c65d"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.5836271,
+                    51.4487267,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "engine-shed",
+                "styleUrl": "#icon-503-DB4436-nodesc",
+                "styleHash": "-3b37c65d"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [
+                        -2.5805962,
+                        51.4493936,
+                        0
+                    ],
+                    [
+                        -2.5810361000000004,
+                        51.4496544,
+                        0
+                    ],
+                    [
+                        -2.5811152,
+                        51.4496193,
+                        0
+                    ],
+                    [
+                        -2.5812373,
+                        51.4496878,
+                        0
+                    ],
+                    [
+                        -2.5825703,
+                        51.4490768,
+                        0
+                    ],
+                    [
+                        -2.5835507,
+                        51.4486447,
+                        0
+                    ],
+                    [
+                        -2.5845438,
+                        51.44914870000001,
+                        0
+                    ],
+                    [
+                        -2.584767,
+                        51.4493936,
+                        0
+                    ],
+                    [
+                        -2.5850071,
+                        51.44940950000001,
+                        0
+                    ],
+                    [
+                        -2.5851808,
+                        51.449521000000004,
+                        0
+                    ],
+                    [
+                        -2.5861846,
+                        51.4497504,
+                        0
+                    ],
+                    [
+                        -2.5862194,
+                        51.4492841,
+                        0
+                    ],
+                    [
+                        -2.5865922000000006,
+                        51.4491855,
+                        0
+                    ],
+                    [
+                        -2.5874558,
+                        51.4492224,
+                        0
+                    ],
+                    [
+                        -2.5878418,
+                        51.4491558,
+                        0
+                    ],
+                    [
+                        -2.5884262,
+                        51.44919310000001,
+                        0
+                    ],
+                    [
+                        -2.5885113,
+                        51.4490604,
+                        0
+                    ],
+                    [
+                        -2.5886172,
+                        51.4489921,
+                        0
+                    ],
+                    [
+                        -2.5888289,
+                        51.4489961,
+                        0
+                    ],
+                    [
+                        -2.5890646000000004,
+                        51.4489003,
+                        0
+                    ],
+                    [
+                        -2.5896809,
+                        51.44899620000001,
+                        0
+                    ],
+                    [
+                        -2.5899263,
+                        51.4486598,
+                        0
+                    ]
+                ]
+            },
+            "properties": {
+                "name": "walk-route"
+            }
+        }
+    ]
+}
+
--- public_html/urban_food_trail_leaflet_v2.js
+++ public_html/urban_food_trail_leaflet_v2.js
@@ -11,7 +11,8 @@
  */
 
 /* global L */
-/* global trail_data */
+/* global trailGeoJson */
+/* global trailInfo */
 
 // Set up map
 var mapCenter = [51.4493, -2.5806];
@@ -42,17 +43,30 @@ var greenIcon = L.icon({
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
+// Style for recommended walking route (polyline vector)
+var trailStyle = {
+    "color": "#008000",
+    "dashArray": "5, 5",
+    "weight": 3,
+    "opacity": 0.65
+};
+
 // Load GeoJSON data (exported from Google as KML & converted to JSON, hacked to JSON-P)
-trail_layer = L.geoJson(trail_data, {
+trail_layer = L.geoJson(trailGeoJson, {
+    style: trailStyle,
     pointToLayer: function (feature, latlng) {
         return L.marker(latlng, {icon: greenIcon});
     },
     onEachFeature: function onEachFeature(feature, layer) {
-        var popupContent = "<strong>" + feature.properties.name +
-                "</strong><br>" + feature.properties.description +
-                "<br><button type='button' onclick=leftSidebar.show()>" +
+        var info = trailInfo[feature.properties.name];
+        if (info !== undefined && info !== null) {
+            var popupContent = "<strong>" + info.fullname +
+                    "</strong><br>" + info.summary +
+                "<br><button type='button' onclick='updatesidebarL(\"" +
+                feature.properties.name + "\");leftSidebar.show()'>" +
                 "More Info</button>";
-        layer.bindPopup(popupContent);
+            layer.bindPopup(popupContent);
+        }
     }
 });
 
@@ -66,9 +80,49 @@ var leftSidebar = L.control.sidebar('sidebarL', {
 map.addControl(leftSidebar);
 leftSidebar.show();
 
+// Get a copy of the introduction text
+trailInfo.intro.details = document.getElementById("sidebarL").innerHTML;
+
 var rightSidebar = L.control.sidebar('sidebarR', {
     closeButton: true,
     position: 'right',
     autoPan: false
 });
 map.addControl(rightSidebar);
+
+function updatesidebarL(locationID) {
+    var headerStr = trailInfo[locationID]["fullname"];
+    var infoStr = "";
+    if (headerStr !== "INTRODUCTION") {
+        /*** TBD: remove when button to open right sidebar exists ***/
+        infoStr += '<p><img src="img/Urban-Food-Growing-Trail.png"' +
+                ' alt="Urban Food Growing Trail"' +
+                ' onclick="leftSidebar.hide();rightSidebar.show()"></p>';
+         /*** TODO: remove when button to open right sidebar exists ***/
+         
+       infoStr += "<h3>" + headerStr + "</h3>";
+    }
+    infoStr += trailInfo[locationID]["details"];
+    var mainLink = trailInfo[locationID]["link"];
+    if (mainLink !== undefined) {
+        infoStr += "<p><a href='" + mainLink + "' target='_blank'>" +
+                "(Further location info on main website)</a></p>";
+    }
+    document.getElementById("sidebarL").innerHTML = infoStr;
+}
+
+function selectLocation(locationID) {
+    rightSidebar.hide();
+    updatesidebarL(locationID);
+    leftSidebar.show();
+}
+
+// Add buttons to Right Sidebar
+var buttonStr = '';
+for (var locationID in trailInfo) {
+    buttonStr += '<p><button type="button"' +
+            'onclick=selectLocation("' + locationID + '")>' +
+            trailInfo[locationID]["fullname"] + '</button></p>';
+}
+document.getElementById("sidebarButtons").innerHTML = buttonStr;
+               
